349ef97dfd0d6db6ff534a5d569f12c2fe509d55
Jean C. Solís <jcsolisu2000@gmail.com>
2024-02-24 21:04:13 -0600

    Comentarios

--- src/app/ejemplo3/ejemplo3.component.ts
+++ src/app/ejemplo3/ejemplo3.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, UntypedFormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-ejemplo3',
@@ -14,6 +14,9 @@ export class Ejemplo3Component {
     user: FormControl<string | null>;
     password: FormControl<string | null>;
   }>;
+  
+  // userForm: any;
+  // userForm: UntypedFormGroup;
 
   constructor(fb: FormBuilder) {
     this.userForm = fb.group({
@@ -26,6 +29,10 @@ export class Ejemplo3Component {
     console.log(this.userForm.value);
     this.userForm.value.password='*'; // esto no funciona
 
-    this.userForm.controls.password.setValue('*');
+    // Esto requiere que userForm sea de tipo FormGroup<T> o el tipo any.
+    // this.userForm.controls.password.setValue('*');
+
+    // Esto funciona cuando userForm tiene cualquier tipo.
+    this.userForm.patchValue({password: '*'});
   }
 }
