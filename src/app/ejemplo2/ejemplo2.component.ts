f8179f78bdade4a76adad91f0dbd7b382758c9da
Jean C. Solís <jcsolisu2000@gmail.com>
2024-02-24 19:35:05 -0600

    Terminar ejuemplo2

--- src/app/ejemplo2/ejemplo2.component.ts
+++ src/app/ejemplo2/ejemplo2.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { UserModel } from './user.model';
 
 @Component({
   selector: 'app-ejemplo2',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './ejemplo2.component.html',
   styleUrl: './ejemplo2.component.scss'
 })
 export class Ejemplo2Component {
+  user: UserModel = {
+    user: '',
+    password: ''
+  }
 
+  submit(): void {
+    console.log(this.user);
+    this.user.password = '*';
+  }
 }
