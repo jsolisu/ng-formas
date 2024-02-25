73a91ad203fc31bf6152018321436aaac246c3b4
Jean C. Solís <jcsolisu2000@gmail.com>
2024-02-24 20:06:38 -0600

    Terminar ejuemplo3

--- src/app/ejemplo3/ejemplo3.component.ts
+++ src/app/ejemplo3/ejemplo3.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejemplo3',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './ejemplo3.component.html',
   styleUrl: './ejemplo3.component.scss'
 })
 export class Ejemplo3Component {
+  userForm: FormGroup<{
+    user: FormControl<string | null>;
+    password: FormControl<string | null>;
+  }>;
 
+  constructor(fb: FormBuilder) {
+    this.userForm = fb.group({
+      user: '',
+      password: ''
+    });
+  }
+
+  submit(): void {
+    console.log(this.userForm.value);
+    this.userForm.value.password='*'; // esto no funciona
+
+    this.userForm.controls.password.setValue('*');
+  }
 }
