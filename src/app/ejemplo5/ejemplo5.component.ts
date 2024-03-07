f72ff18e3160ab9f29b49aee65353433cb89e883
Jean C. Solís <jcsolisu2000@gmail.com>
2024-03-07 17:55:59 -0600

    Agregar validacion del ejemplo5

--- src/app/ejemplo5/ejemplo5.component.ts
+++ src/app/ejemplo5/ejemplo5.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl, FormBuilder, FormControl, FormGroup,
+  ReactiveFormsModule, ValidationErrors, Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-ejemplo5',
@@ -19,9 +22,19 @@ export class Ejemplo5Component {
   constructor(fb: FormBuilder) {
     this.userForm = fb.group({
       user: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(32)]],
-      nombre: '',
+      nombre: ['', Ejemplo5Component.usuarioCompleto],
       password: fb.control('', Validators.required)
     });
+  }
+
+  // static = Compartido entre instancias de la clase, es decir, permite utilizar
+  // Ejemplo5Component.usuarioCompleto sin necesidad de instanciar la clase.
+  //
+  // El metodo retorna null si no hay error, en caso contrario un objeto que describe el error
+  static usuarioCompleto(control: AbstractControl): ValidationErrors | null {
+    // contar dos palabras donde cada una tenga al menos 1 caracter
+    return control.value.split(' ').filter((p: string) => p.trim().length > 0)
+      .length < 2 ? { incompleto: true } : null
   }
 
   submit(): void {
