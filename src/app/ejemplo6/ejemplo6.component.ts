ad6dbc5f22deb4a0459ee3aac218d8dae7641874
Jean C. Solís <jcsolisu2000@gmail.com>
2024-03-10 13:21:20 -0600

    Finalizacion ejemplo6

--- src/app/ejemplo6/ejemplo6.component.ts
+++ src/app/ejemplo6/ejemplo6.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  AbstractControl, FormBuilder, FormControl, FormGroup,
+  ReactiveFormsModule, ValidationErrors, Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-ejemplo6',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './ejemplo6.component.html',
   styleUrl: './ejemplo6.component.scss'
 })
 export class Ejemplo6Component {
+  userForm: FormGroup<{
+    user: FormControl<string | null>;
+    passwordGroup: FormGroup<{
+      password: FormControl<string | null>;
+      passwordConfirm: FormControl<string | null>;
+    }>
+  }>;
 
+  constructor(fb: FormBuilder) {
+    this.userForm = fb.group({
+      user: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(32)]],
+      passwordGroup: fb.group(
+        {
+          password: fb.control('', Validators.required),
+          passwordConfirm: fb.control('', Validators.required),
+        },
+        { validators: Ejemplo6Component.passwordIgual })
+    });
+  }
+
+  static passwordIgual(group: AbstractControl): ValidationErrors | null {
+    return group.value.password === group.value.passwordConfirm ? null : { errorIgualdad: true };
+  }
+
+  submit(): void {
+  }
 }
